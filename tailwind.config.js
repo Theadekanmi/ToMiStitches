c1f95ccef8b573b7c3ba823cefb3ee73420a5e25
Theadekanmi <theadekanmi@gmail.com>
2025-06-03 05:18:46 -0500

    Clean initial commit without node_modules or large files

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html" // 👈 Looks through all HTML in the public folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
